a9e864ed4ac2aace5039d4f333bbbdca69649867
Berat Dilki <bd154494@renishaw.com>
2024-11-05 13:47:55 +0000

    Add database context

---
+++ LibraryManagementAPI/Data/LibraryContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryManagementAPI.Models;
+
+namespace LibraryManagementAPI.Data;
+
+public class LibraryContext : DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+
+    public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>()
+            .HasMany(a => a.Books)
+            .WithOne(b => b.Author)
+            .HasForeignKey(b => b.AuthorId);
+
+        modelBuilder.Entity<Book>()
+            .HasMany(b => b.Loans)
+            .WithOne(l => l.Book)
+            .HasForeignKey(l => l.BookId);
+
+        // Seed data
+        
+    }
+}
