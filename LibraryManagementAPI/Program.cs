8619ff99011b914b30c19512d8010ddc33a41b96
Berat Dilki <bd154494@renishaw.com>
2024-11-05 13:48:40 +0000

    add database context service

--- LibraryManagementAPI/Program.cs
+++ LibraryManagementAPI/Program.cs
@@ -1,6 +1,10 @@
+using LibraryManagementAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add database context
+builder.Services.AddDbContext<LibraryContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
