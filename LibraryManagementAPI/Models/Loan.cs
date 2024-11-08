a6afe7580de7af25219b03a85d1c0d0f057b80d6
Berat Dilki <bd154494@renishaw.com>
2024-11-08 16:50:28 +0000

    remove Loan model

--- LibraryManagementAPI/Models/Loan.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibraryManagementAPI.Models;
-
-public class Loan
-{
-    public int Id { get; set; }
-    public int BookId { get; set; }
-    public DateTime LoanDate { get; set; }
-    public Book Book { get; set; } = new Book();
-}
