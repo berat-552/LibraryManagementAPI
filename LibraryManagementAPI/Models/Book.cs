d125b58476fef1544294959e73f1e08a6894c4cf
Berat Dilki <bd154494@renishaw.com>
2024-11-09 21:48:48 +0000

    add GetTestBooks static method

--- LibraryManagementAPI/Models/Book.cs
+++ LibraryManagementAPI/Models/Book.cs
@@ -5,9 +5,41 @@ public class Book
     public int Id { get; set; }
     public string BookTitle { get; set; } = string.Empty;
     public string ISBN { get; set; } = string.Empty; // ISBN - International Standard Book Number
-    public string Genre {  get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
     public DateTime PublishedDate { get; set; }
     public int AuthorId { get; set; }
-    public Author Author { get; set; } = new Author();
-    public ICollection<Loan> Loans { get; set; } = new List<Loan>();
+
+    public static List<Book> GetTestBooks()
+    {
+        return
+            [
+                new Book
+                {
+                    Id = 1,
+                    BookTitle = "Pride and Prejudice",
+                    ISBN = "978-3-16-148410-0",
+                    Genre = "Romance",
+                    AuthorId = 1,
+                    PublishedDate = new DateTime(1813, 1, 28),
+                },
+                new Book
+                {
+                    Id = 2,
+                    BookTitle = "Adventures of Huckleberry Finn",
+                    ISBN = "978-0-14-243717-9",
+                    Genre = "Adventure",
+                    AuthorId = 2,
+                    PublishedDate = new DateTime(1884, 12, 10),
+                },
+                new Book
+                {
+                    Id = 3,
+                    BookTitle = "Great Expectations",
+                    ISBN = "978-0-14-143956-3",
+                    Genre = "Fiction",
+                    AuthorId = 3,
+                    PublishedDate = new DateTime(1861, 8, 1),
+                }
+            ];
+    }
 }
