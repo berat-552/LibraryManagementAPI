42da2a52dc8be8d6fd97da8b7dacc491c1a03c62
Berat Dilki <bd154494@renishaw.com>
2024-11-13 14:47:52 +0000

    add model tests

---
+++ LibraryManagementAPI.Tests/Models/AuthorTests.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagementAPI.Models;
+
+namespace LibraryManagementAPI.Tests.Models;
+
+public class AuthorTests
+{
+    [Fact]
+    public void Author_ShouldHaveDefaultValues()
+    {
+        var author = new Author();
+
+        Assert.Equal(string.Empty, author.AuthorName);
+        Assert.Equal(string.Empty, author.Biography);
+        Assert.Equal([], author.Books);
+    }
+
+    [Fact]
+    public void GetTestAuthors_ShouldReturnListOfAuthors()
+    {
+        var count = 6;
+        var authors = Author.GetTestAuthors();
+
+        Assert.NotNull(authors);
+        Assert.Equal(count, authors.Count);
+        Assert.IsType<List<Author>>(authors);
+    }
+}
---
+++ LibraryManagementAPI.Tests/Models/BookTests.cs
@@ -0,0 +1,29 @@
+﻿using LibraryManagementAPI.Models;
+
+namespace LibraryManagementAPI.Tests.Models;
+
+public class BookTests
+{
+    [Fact]
+    public void Book_ShouldHaveDefaultValues()
+    {
+        var book = new Book();
+
+        Assert.Equal(string.Empty, book.BookTitle);
+        Assert.Equal(string.Empty, book.ISBN);
+        Assert.Equal(string.Empty, book.Genre);
+        Assert.Equal(default, book.PublishedDate);
+        Assert.Equal(0, book.AuthorId);
+    }
+
+    [Fact]
+    public void GetTestBooks_ShouldReturnListOfBooks()
+    {
+        var count = 3;
+        var books = Book.GetTestBooks();
+
+        Assert.NotNull(books);
+        Assert.Equal(count, books.Count);
+        Assert.IsType<List<Book>>(books);
+    }
+}
