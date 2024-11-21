492454b8da0fbba8ef8fbed28027656d4077d1e0
Berat Dilki <bd154494@renishaw.com>
2024-11-21 15:26:39 +0000

    fix format

--- LibraryManagementAPI/Data/LibraryContext.cs
+++ LibraryManagementAPI/Data/LibraryContext.cs
@@ -3,20 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagementAPI.Data;
 
-    public class LibraryContext(DbContextOptions<LibraryContext> options) : DbContext(options)
+public class LibraryContext(DbContextOptions<LibraryContext> options) : DbContext(options)
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public DbSet<Author> Authors { get; set; }
-        public DbSet<Book> Books { get; set; }
+        modelBuilder.Entity<Author>()
+            .HasMany(a => a.Books)
+            .WithOne()
+            .HasForeignKey(b => b.AuthorId);
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        if (!Database.IsInMemory())
         {
-            modelBuilder.Entity<Author>()
-                .HasMany(a => a.Books)
-                .WithOne()
-                .HasForeignKey(b => b.AuthorId);
-
-            if (!Database.IsInMemory())
-            {
             SeedData.Seed(modelBuilder);
         }
     }
