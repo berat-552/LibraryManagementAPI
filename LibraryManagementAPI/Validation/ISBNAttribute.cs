9da9d8ee94b89b0a278ed9622ec78011f2c1d256
Berat Dilki <bd154494@renishaw.com>
2024-11-25 14:12:15 +0000

    use filescoped namespace

--- LibraryManagementAPI/Validation/ISBNAttribute.cs
+++ LibraryManagementAPI/Validation/ISBNAttribute.cs
@@ -1,32 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
-namespace LibraryManagementAPI.Validation
+namespace LibraryManagementAPI.Validation;
+
+public class ISBNAttribute : ValidationAttribute
 {
-    public class ISBNAttribute : ValidationAttribute
+    private const string Pattern = @"^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$";
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        private const string Pattern = @"^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$";
-
-        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        if (value == null)
         {
-            if (value == null)
-            {
-                return ValidationResult.Success; // Consider null as valid. Use [Required] for mandatory fields.
-            }
+            return ValidationResult.Success; // Consider null as valid. Use [Required] for mandatory fields.
+        }
 
-            // if it is a string, assigns it to the variable isbn
-            if (value is not string isbn)
-            {
-                return new ValidationResult("Invalid ISBN format.");
-            }
-
-            var regex = new Regex(Pattern);
-            if (regex.IsMatch(isbn))
-            {
-                return ValidationResult.Success;
-            }
-
+        // if it is a string, assigns it to the variable isbn
+        if (value is not string isbn)
+        {
             return new ValidationResult("Invalid ISBN format.");
         }
+
+        var regex = new Regex(Pattern);
+        if (regex.IsMatch(isbn))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("Invalid ISBN format.");
     }
 }
