cf3b3e648d9068b74c21e3822e4d2981d98b464d
Berat Dilki <bd154494@renishaw.com>
2024-11-28 15:34:51 +0000

    add PasswordHasher tests

---
+++ LibraryManagementAPI.Tests/Helpers/PasswordHasherTests.cs
@@ -0,0 +1,39 @@
+﻿using LibraryManagementAPI.Helpers;
+
+namespace Tests.Helpers;
+
+public class PasswordHasherTests
+{
+    [Fact]
+    public void HashPassword_ShouldReturnHashedPassword()
+    {
+        string password = "securePassword123";
+        string hashedPassword = PasswordHasher.HashPassword(password);
+
+        Assert.False(string.IsNullOrEmpty(hashedPassword));
+        Assert.NotEqual(password, hashedPassword); // Ensure the hashed password is different from the original
+    }
+
+    [Fact]
+    public void VerifyPassword_ShouldReturnTrue_ForValidPassword()
+    {
+        string password = "securePassword123";
+        string hashedPassword = PasswordHasher.HashPassword(password);
+
+        bool isValid = PasswordHasher.VerifyPassword(password, hashedPassword);
+
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void VerifyPassword_ShouldReturnFalse_ForInvalidPassword()
+    {
+        string password = "securePassword123";
+        string wrongPassword = "wrongPassword123";
+        string hashedPassword = PasswordHasher.HashPassword(password);
+
+        bool isValid = PasswordHasher.VerifyPassword(wrongPassword, hashedPassword);
+
+        Assert.False(isValid);
+    }
+}
