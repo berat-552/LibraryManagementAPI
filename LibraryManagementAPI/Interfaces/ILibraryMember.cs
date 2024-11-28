aff2ae9be3c1cdb50a50eebc593c51c8d5518d49
Berat Dilki <bd154494@renishaw.com>
2024-11-28 15:38:11 +0000

    add LibraryMember Interface

--- LibraryManagementAPI/Interfaces/ILibraryMember.cs
+++ LibraryManagementAPI/Interfaces/ILibraryMember.cs
@@ -2,7 +2,7 @@
 
 public interface ILibraryMember
 {
-    Guid Id { get; set; }
+    int Id { get; set; }
     string Username { get; set; }
     string Password { get; set; }
     string Email { get; set; }
