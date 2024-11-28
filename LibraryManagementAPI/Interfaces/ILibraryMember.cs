aa1708ad6261e51e473542d6a55291512fd40fda
Berat Dilki <bd154494@renishaw.com>
2024-11-28 11:28:42 +0000

    add LibraryMemberModel

---
+++ LibraryManagementAPI/Interfaces/ILibraryMember.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementAPI.Interfaces;
+
+public interface ILibraryMember
+{
+    Guid Id { get; set; }
+    string Username { get; set; }
+    string Password { get; set; }
+    string Email { get; set; }
+}
---
+++ LibraryManagementAPI/Models/LibraryMember.cs
@@ -0,0 +1,23 @@
+﻿using LibraryManagementAPI.Interfaces;
+using LibraryManagementAPI.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementAPI.Models;
+
+public class LibraryMember : ILibraryMember
+{
+    public Guid Id { get; set; } = new Guid();
+
+    [Required(ErrorMessage = "userName is required")]
+    [StringLength(100, MinimumLength = 1, ErrorMessage = "username must be between 1 and 100 characters")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "email is required")]
+    [StringLength(100, MinimumLength = 1, ErrorMessage = "email must be between 1 and 100 characters")]
+    [Email]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "password is required")]
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "password must be between 1 and 50 characters")]
+    public string Password { get; set; } = string.Empty;
+}
