5d5c296febd0e9ebd5d0dbc7d86ebd7731c9d14d
Berat Dilki <bd154494@renishaw.com>
2024-11-28 15:36:44 +0000

    add PasswordHasher

---
+++ LibraryManagementAPI/Helpers/PasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿using Bcrypt = BCrypt.Net.BCrypt;
+
+namespace LibraryManagementAPI.Helpers;
+
+public static class PasswordHasher
+{
+    public static string HashPassword(string password) => Bcrypt.HashPassword(password);
+    public static bool VerifyPassword(string password, string hashedPassword) => Bcrypt.Verify(password, hashedPassword);
+}
