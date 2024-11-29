aaa6978f5c7ce890e5b2afa0ebd05717496064e3
Berat Dilki <bd154494@renishaw.com>
2024-11-29 11:56:43 +0000

    use authentication

--- LibraryManagementAPI/Program.cs
+++ LibraryManagementAPI/Program.cs
@@ -1,5 +1,10 @@
 using LibraryManagementAPI.Data;
+using LibraryManagementAPI.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -85,7 +90,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
