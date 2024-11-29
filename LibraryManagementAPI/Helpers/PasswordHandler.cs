c6879aabbeff84b5afc3a3f14a30e89c40138532
Berat Dilki <bd154494@renishaw.com>
2024-11-29 11:52:37 +0000

    rename PasswordHasher to PasswordHandler

--- LibraryManagementAPI/Helpers/PasswordHandler.cs
+++ LibraryManagementAPI/Helpers/PasswordHandler.cs
@@ -2,7 +2,7 @@
 
 namespace LibraryManagementAPI.Helpers;
 
-public static class PasswordHasher
+public static class PasswordHandler
 {
     public static string HashPassword(string password) => Bcrypt.HashPassword(password);
     public static bool VerifyPassword(string password, string hashedPassword) => Bcrypt.Verify(password, hashedPassword);
