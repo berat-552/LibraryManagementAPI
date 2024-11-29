8e0a7099d90d377ad0bf19c638a67d649f4a59d0
Berat Dilki <bd154494@renishaw.com>
2024-11-29 11:53:27 +0000

    add LoginModel

---
+++ LibraryManagementAPI/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementAPI.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementAPI.Models;
+
+public class LoginModel
+{
+    [Required(ErrorMessage = "Email is required")]
+    [Email(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; } = string.Empty;
+}
