871e1c46994e31bfd2035b5bbe1d596cd4b6c535
Berat Dilki <bd154494@renishaw.com>
2024-11-29 11:53:10 +0000

    add authentication handler

---
+++ LibraryManagementAPI/Helpers/AuthenticationHandler.cs
@@ -0,0 +1,38 @@
+﻿using LibraryManagementAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LibraryManagementAPI.Helpers;
+
+public class AuthenticationHandler(IConfiguration configuration)
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public string GenerateJWTToken(LibraryMember user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Name, user.Username)
+        };
+
+        var jwtSecret = _configuration["JWT_Secret"];
+        if (string.IsNullOrEmpty(jwtSecret) || jwtSecret.Length < 32)
+        {
+            throw new InvalidOperationException("JWT secret is not configured properly.");
+        }
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var jwtToken = new JwtSecurityToken(
+            claims: claims,
+            notBefore: DateTime.UtcNow,
+            expires: DateTime.UtcNow.AddDays(30),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+    }
+}
