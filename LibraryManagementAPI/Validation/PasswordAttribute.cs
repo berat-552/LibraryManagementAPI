8f338c2822f7ed1bf2b0f6982b187b26794e80e8
Berat Dilki <bd154494@renishaw.com>
2024-12-11 20:26:44 +0000

    add password validation attribute

---
+++ LibraryManagementAPI/Validation/PasswordAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace LibraryManagementAPI.Validation;
+
+public class PasswordAttribute : ValidationAttribute
+{
+    private const string Pattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?"":{}|<>])[A-Za-z\d!@#$%^&*(),.?"":{}|<>]{8,}$";
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return ValidationResult.Success; // Consider null as valid. Use [Required] for mandatory fields.
+        }
+
+        if (value is not string password)
+        {
+            return new ValidationResult("Password must contain at least 8 characters, one special character, one uppercase letter, one lowercase letter and one number.");
+        }
+
+        var regex = new Regex(Pattern);
+        if (regex.IsMatch(password))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("Password must contain at least 8 characters, one special character, one uppercase letter, one lowercase letter and one number.");
+    }
+}
